0ce8b7f536e25044db862ccc1e464e239ecde725
AlihanCelik <alihan_08032003@hotmail.com>
2024-08-03 21:23:54 +0300

    foodApp

--- app/src/main/java/com/example/foodapp/MainActivity.kt
+++ app/src/main/java/com/example/foodapp/MainActivity.kt
@@ -5,11 +5,16 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        val bottomNavigationView=findViewById<BottomNavigationView>(R.id.btm_nav)
+        val navController=Navigation.findNavController(this,R.id.host_fragment)
+        NavigationUI.setupWithNavController(bottomNavigationView,navController)
     }
 }
